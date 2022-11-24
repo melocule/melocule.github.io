b0bd4f130b2b8df5ff1b8f803f6a173738308619
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-11-23 23:53:37 -0500

    fix: better portrait

--- components/Home-page.vue
+++ components/Home-page.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 .flex.flex-col.grow.justify-between
-    div.flex.justify-self-center.justify-content-center.align-items-center.grow
-        div.hero
-            div(class='md:flex-row-reverse').hero-content.flex-col
-                img(src='../assets/photo.jpeg').max-w-sm.rounded-lg.shadow-2xl
-                div(class='md:pr-12 md:py-0').py-8
-                    h1.text-5xl I'm Melody Liu 👋
-                    p.py-6 Full stack developer for web and mobile. <br> I typically use React, Typescript, and Tailwind.
-    footer-melo
+  .flex.justify-self-center.justify-content-center.align-items-center.grow
+    .hero
+      .hero-content.flex-col(class='md:flex-row-reverse')
+        img.max-w-sm.rounded-lg.shadow-2xl(src='../assets/photo.jpeg')
+        .py-8(class='md:pr-12 md:py-0')
+          h1.text-5xl I'm Melody Liu 👋
+          p.py-6 Full stack developer for web and mobile. <br> I typically use React, Typescript, and Tailwind.
+  footer-melo
 </template>
--- components/Nav-bar.vue
+++ components/Nav-bar.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
 header.pt-12.pb-4
-  div.navbar.py-0.px-14
+  .navbar.py-0.px-14
     .flex-1 
-      a(href=".").btn.btn-ghost.normal-case.text-4xl.fancy-font ml.
+      a.btn.btn-ghost.normal-case.text-4xl.fancy-font(href='.') &#60;ml/&#62;
     .flex-none 
       ul.menu.menu-horizontal
         li
-          NuxtLink(to="/resume") Resume
+          NuxtLink(to='/resume') Resume
         li 
-          a(href="mailto: melody.liu.2@gmail.com" target="_blank") Contact
+          a(href='mailto: melody.liu.2@gmail.com', target='_blank') Contact
 </template>
 
 <style>
 .fancy-font {
-    font-family: Lobster, sans-serif;
+  font-family: Lobster, sans-serif;
 }
 </style>
