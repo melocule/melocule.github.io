3f5632d1e1e58058ec3b6f74114b3d97b9ecca91
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-12-23 22:20:40 -0500

    Chore: Remove unnecessary components

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,3 +1,10 @@
 <template lang="pug">
-resume-embed
+div
+  .overflow-auto
+    iframe.rounded-lg.drop-shadow-lg.m-auto(
+      width='860',
+      height='1100',
+      src='https://docs.google.com/document/d/1rDXh_hgSNXl5VThkDCUiLw20CjVE0LtUuKBGOH9COlE/preview'
+    )
+  footer-melo
 </template>
